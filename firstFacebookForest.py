6fda5a3998498a7f6e9930965a1b008311e81d6b
rmcdonnell <rmcdonnell1990@icloud.com>
2015-06-05 18:39:36 -0400

    first commit

---
+++ firstFacebookForest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 18:32:30 2015
+
+@author: bmcdonnell
+"""
+
+import csv as csv
+import numpy as np
+import pandas as pd
+
+train = []
+train_csv_file_obj = csv.reader(open('train.csv', 'rb'))
+for row in train_csv_file_obj:
+    train.append(row)
+    
+test = []
+test_csv_file_obj = csv.reader(open('test.csv', 'rb'))
+for row in test_csv_file_obj:
+    test.append(row)
+
